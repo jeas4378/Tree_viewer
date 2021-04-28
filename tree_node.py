0eb5869c215898f7f32461b1108abb80f3b69a99
jeas4378 <jeas4378@student.su.se>
2021-04-28 11:40:06 +0200

    Uppdaterat 'tree_node' med nya värden

--- tree_node.py
+++ tree_node.py
@@ -3,7 +3,8 @@ class Node:
     def __init__(self):
         self.left_child = None
         self.right_child = None
-        self.name = ""
+        self.id = "Null"
+        self.level = -1.00
 
     def set_left_child(self, node):
         self.left_child = node
@@ -19,8 +20,17 @@ class Node:
         if self.right_child is not None:
             return self.right_child
 
-    def set_name(self, str_name):
-        self.name = str_name
+    def set_level(self, flt_level):
+        self.level = flt_level
+
+    def set_id(self, str_id):
+        self.id = str_id
+
+    def get_level(self):
+        return self.level
+
+    def get_id(self):
+        return self.id
 
 
 class Tree:
